bed7a952106ce0960b9c199235dbcb586caf38cc
wu.kai <wu.kai@navercorp.com>
2022-01-13 17:20:13 +0800

    add tinymce editor

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -2,14 +2,23 @@
   <div id="home">
     {{$t('message')}}
     Home
-    <div>
+
+    <TEditor></TEditor>
+    <div class="editor-box">
       <router-view></router-view>
     </div>
   </div>
 </template>
+<script lang="ts" setup>
+import TEditor from './editor.vue';
+
+</script>
 <style lang="less" scoped>
   #home{
     padding: 60px;
     text-align: center;
+  }
+  .editor-box{
+    margin: 20px 0;
   }
 </style>
